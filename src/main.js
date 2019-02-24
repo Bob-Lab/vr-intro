e0865c5b6d06555b8792dd1c790b52576ff76670
楊竑昕 <jk195417@gmail.com>
2019-02-24 20:44:20 +0800

    add menu

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ import './assets/js/import_jquery.js'
 import 'bootstrap'
 import "@fancyapps/fancybox/dist/jquery.fancybox.js"
 
+function show(selector) {
+  $('.section').removeClass('d-flex')
+  $('.section').addClass('d-none')
+  $(selector).removeClass('d-none')
+  $(selector).addClass('d-flex')
+}
+
+function showContentByHash() {
+  if (window.location.hash === '#boracay') {
+    show("#boracay")
+  } else if (window.location.hash === '#guam') {
+    show("#guam")
+  } else {
+    show("#menu")
+  }
+}
+
 $(document).ready(() => {
+  showContentByHash()
+  window.addEventListener('hashchange', () => showContentByHash())
+
   $('.video-btn').click(function() {
     const videoId = $(this).attr('data-video-id')
     const url = `https://www.youtube.com/embed/${videoId}`
